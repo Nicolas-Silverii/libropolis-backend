2e739871c966dee9be7f60c7cc550eed933daf98
Nicolàs <nicolassilverii@gmail.com>
2025-11-14 20:52:38 -0300

    DTO CreateLibroDto: validaciones con class-validator para la creación de libros

---
+++ src/libros/dto/create-libro.dto.ts
@@ -0,0 +1,30 @@
+import { IsString, IsOptional, IsBoolean, IsInt, IsUrl } from "class-validator";
+
+export class CreateLibroDto {
+@IsString()
+titulo: string;
+
+@IsOptional()
+@IsString()
+autor?: string;
+
+@IsOptional()
+@IsUrl()
+imagen_url?: string;
+
+@IsOptional()
+@IsString()
+descripcion?: string;
+
+@IsOptional()
+@IsInt()
+anio?: number;
+
+@IsOptional()
+@IsBoolean()
+favorito?: boolean;
+
+@IsInt()
+usuario_id: number;
+
+}
