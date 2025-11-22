e6fd806a29e8db2b60ddc72c0057bcb040a3a778
Nicolàs <nicolassilverii@gmail.com>
2025-11-22 18:10:27 -0300

    db: ajuste en Libro entity para relación con Usuario

---
+++ src/entidades/libro.entity.ts
@@ -0,0 +1,29 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Usuario } from "./usuario.entity";
+
+@Entity('Libro')
+export class Libro {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 150 })
+  titulo: string;
+
+  @Column({ length: 100, nullable: true })
+  autor: string;
+
+  @Column({ default: false })
+  favorito: boolean;
+
+  @Column({ type: 'text', nullable: true })
+  imagen_url: string;
+
+  @Column({ type: 'text', nullable: true })
+  descripcion: string;
+
+  @Column({ type: 'int', nullable: true })
+  anio: number;
+
+  @ManyToOne (() => Usuario, (usuario) => usuario.libros, { onDelete: 'CASCADE'})
+  usuario: Usuario;
+}
