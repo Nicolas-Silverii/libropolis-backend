cf313923d2102a74da93a90c6b36796c6bbbabd1
Nicolàs <nicolassilverii@gmail.com>
2025-11-25 03:04:14 -0300

    LibrosModule con controlador, servicio y entidades Libro/Usuario integrados

--- src/libros/libros.module.ts
+++ src/libros/libros.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { LibrosService } from './libros.service';
 import { LibrosController } from './libros.controller';
 import { Libro } from '../entidades/libro.entity';
+import { Usuario } from "../entidades/usuario.entity";
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Libro]), HttpModule],
+  imports: [TypeOrmModule.forFeature([Libro, Usuario]), HttpModule],
   controllers: [LibrosController],
   providers: [LibrosService],
 })
