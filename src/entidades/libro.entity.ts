a08f9057ef931c0e350f412214dd82c5a5739491
Nicolàs <nicolassilverii@gmail.com>
2025-11-29 18:03:15 -0300

    Normalizacionde  entidades libro y usuario con relaciones y tipos consistentes

--- src/entidades/libro.entity.ts
+++ src/entidades/libro.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
 import { Usuario } from "./usuario.entity";
+import { IsNotEmpty, IsInt, maxLength, MaxLength } from 'class-validator';
 
 @Entity('libros')
 export class Libro {
@@ -7,9 +8,12 @@ export class Libro {
   id: number;
 
   @Column({ length: 150 })
+  @IsNotEmpty({message: 'El título es obligatorio'})
+  @MaxLength(150, {message: 'El título no puede superar los 150 caracteres'})
   titulo: string;
 
   @Column({ length: 100, nullable: true })
+  @MaxLength(100, {message: 'El autor no puede superar los 100 caracteres'})
   autor: string;
 
   @Column({ default: false })
@@ -22,6 +26,7 @@ export class Libro {
   descripcion: string;
 
   @Column({ type: 'int', nullable: true })
+  @IsInt({ message: 'El año debe ser un número válido'})
   anio: number;
 
   @ManyToOne (() => Usuario, (usuario) => usuario.libros, { onDelete: 'CASCADE'})
--- src/entidades/usuario.entity.ts
+++ src/entidades/usuario.entity.ts
@@ -1,5 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { Libro } from './libro.entity';
+import { IsEmail, IsNotEmpty, MinLength, MaxLength } from 'class-validator';
+
 
 @Entity('usuarios')
 export class Usuario {
@@ -7,16 +9,23 @@ export class Usuario {
   id: number;
 
   @Column({ length: 100 })
+  @IsNotEmpty({ message: 'El nombre es obligatorio'})
+  @MaxLength(100, { message: 'El nombre no puede superar los 100 caracteres'})
   nombre: string;
 
   @Column({ length: 100 })
+  @IsNotEmpty({ message: 'El apellido es obligatorio'})
+  @MaxLength(100, { message: 'El apellido no puede superar los 100 caracteres'})
   apellido: string;
 
   @Column({ unique: true })
+  @IsEmail( {}, {message: 'El email debe tener formato válido'})
   email: string;
 
   @Column()
-  contraseña: string;
+  @IsNotEmpty({ message: 'La contraseña es obligatoria'})
+  @MinLength( 8,{ message: ' La contraseña debe tener al menos 8 caracteres'})
+  password: string;
 
   @Column({ default: 'lector' })
   rol: string;
